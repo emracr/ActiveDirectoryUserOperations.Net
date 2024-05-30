d43c7b2fb51ea102f3e5dfef8b7b2dd124a07e38
emracr <emrullah.acar444@gmail.com>
2024-05-30 14:11:26 +0300

    Add project files.

---
+++ ActiveDirectoryTest/ActiveDirectoryUserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ActiveDirectoryTest
+{
+    public class ActiveDirectoryUserModel
+    {
+        public string Domain { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AdminUserName { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
---
+++ ActiveDirectoryTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using ActiveDirectoryTest;
+using System.DirectoryServices.AccountManagement;
+
+class Program
+{
+    static void Main()
+    {
+        ActiveDirectoryUserModel activeDirectoryUserModel = new()
+        {
+            Domain = "domain.com",
+            Email = "email@email.com",
+            UserName = "username",
+            Password = "password",
+            AdminUserName = "username",
+            AdminPassword = "password"
+        };
+
+        bool result = Login(activeDirectoryUserModel);
+
+        if (result)
+        {
+            Console.WriteLine("Sisteme hoşgeldiniz");
+        }
+        else
+        {
+            Console.WriteLine("Kullanıcı adı veya parola hatalı!");
+        }
+    }
+
+    static bool Login(ActiveDirectoryUserModel activeDirectoryUser)
+    {
+        using (PrincipalContext context = new(ContextType.Domain, activeDirectoryUser.Domain))
+        {
+            return context.ValidateCredentials(activeDirectoryUser.UserName, activeDirectoryUser.Password);
+        }
+    }
+
+    static bool CheckIfUserIsExistByUserName(ActiveDirectoryUserModel activeDirectoryUser)
+    {
+        using (PrincipalContext context = new PrincipalContext(ContextType.Domain, activeDirectoryUser.Domain, activeDirectoryUser.AdminUserName, activeDirectoryUser.AdminPassword))
+        {
+            UserPrincipal user = UserPrincipal.FindByIdentity(context, activeDirectoryUser.UserName);
+            return user != null;
+        }
+    }
+
+    static bool CheckIfUserIsExistByEmail(ActiveDirectoryUserModel activeDirectoryUser)
+    {
+        using (PrincipalContext context = new PrincipalContext(ContextType.Domain, activeDirectoryUser.Domain, activeDirectoryUser.AdminUserName, activeDirectoryUser.AdminPassword))
+        {
+            UserPrincipal userPrincipal = new UserPrincipal(context)
+            {
+                EmailAddress = activeDirectoryUser.Email
+            };
+
+            using (PrincipalSearcher searcher = new PrincipalSearcher(userPrincipal))
+            {
+                UserPrincipal result = (UserPrincipal)searcher.FindOne();
+                return result != null;
+            }
+        }
+    }
+}
